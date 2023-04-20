ca38aa54757e54fd2e2d736af1e307492883344e
Nahom Abraham <nahomabraham17@gmail.com>
2023-04-20 18:59:52 +0300

    add more styles to checked todo

--- src/Todo.jsx
+++ src/Todo.jsx
@@ -3,8 +3,8 @@ import React from "react";
 export default function Todo(props){
     return (
         <div className="todo" onClick={props.toggle}>
-            <div className={!props.checked ? "todo-checkbox": "todo-checkbox-checked"}></div>
-            <div className="todo-text">{props.text}</div>
+            <div className={!props.checked ? "todo-checkbox" : "todo-checkbox-checked"}></div>
+            <div className={!props.checked ? "todo-text" : "todo-text-completed"}>{props.text}</div>
         </div>
     )
 }
