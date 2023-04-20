8fd40d58317750c83088fc3171ec9a38a315d03a
Nahom Abraham <nahomabraham17@gmail.com>
2023-04-20 18:53:49 +0300

    add toggle function for checkbox

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,38 @@
 import React from "react";
+import {nanoid} from "nanoid";
 import Todo from "./Todo"
 import TopBar from "./TopBar"
 
 export default function App(){
     const [todos, setTodos] = React.useState([])
     
-    let todosElements = todos.map(todo => <Todo text={todo.text} />)
+    let todosElements = todos.map(todo => 
+        <Todo 
+            key={todo.id}
+            id={todo.id}
+            text={todo.text}
+            checked={todo.checked}
+            toggle={() => toggleCheckbox(todo.id)}
+        />
+    )
 
     function addTodo(){
         setTodos(prevTodos => [
             ...prevTodos,
-            {text: "Do this"}
+            {text: "Do this", checked: false, id: nanoid()}
         ])
+    }
+
+    function toggleCheckbox(id){
+        setTodos(prevTodos => prevTodos.map(prevTodo => (
+            prevTodo.id === id ?
+            {...prevTodo, checked: !prevTodo.checked} :
+            prevTodo
+            )))
     }
     return (
         <div className="main">
-            <TopBar addTodo={addTodo}/>
+            <TopBar addTodo={addTodo} />
             <div className="todo-container">
                 {todosElements}
             </div>
--- src/Todo.jsx
+++ src/Todo.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 export default function Todo(props){
     return (
-        <div className="todo">
-            <div className="todo-checkbox"></div>
+        <div className="todo" onClick={props.toggle}>
+            <div className={!props.checked ? "todo-checkbox": "todo-checkbox-checked"}></div>
             <div className="todo-text">{props.text}</div>
         </div>
     )
