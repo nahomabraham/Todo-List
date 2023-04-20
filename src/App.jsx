37ffc1efa78c05c79e2d2d93108ba8c6c14b1bbb
Nahom Abraham <nahomabraham17@gmail.com>
2023-04-20 12:37:25 +0300

    design body box and add input and button and style them

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function App(){
+    return (
+        <div className="main">
+            <div className="inputs-container">
+                <input type="text" placeholder="New task..."/>
+                <button>ADD</button>
+            </div>
+        </div>
+    )
+}
